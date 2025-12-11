4a81ec2854dd36f6fac906c5478986de7f9d9096
Alexander <lsndrme@ya.ru>
2025-12-12 01:38:35 +0700

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/delete-api-file-path-config-products-crystals-some-file-jpg.test.ts
+++ .brightsec/tests/delete-api-file-path-config-products-crystals-some-file-jpg.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('DELETE /api/file?path=config/products/crystals/some_file.jpg', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['lfi', 'ssrf', 'bopla', 'http_method_fuzzing'],
+      tests: ['lfi'],
       attackParamLocations: [AttackParamLocation.QUERY],
       starMetadata: {
         code_source: "lsndr/pureflow2:stable",
