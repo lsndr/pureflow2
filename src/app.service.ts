92e4c0049e52c30016c562459816ba92434ab70c
Alexander <lsndrme@ya.ru>
2025-12-13 17:02:20 +0700

    fix: apply automated fixes for detected vulnerabilities

--- src/app.service.ts
+++ src/app.service.ts
@@ -66,7 +66,7 @@ export class AppService {
       awsBucket: this.configService.get<string>(
         AppModuleConfigProperties.ENV_AWS_BUCKET
       ),
-      sql: `postgres://${dbUser}:${dbPwd}@${dbHost}:${dbPort}/${dbSchema} `, // Use environment variable for password
+      sql: `postgres://${dbUser}:${dbPwd}@${dbHost}:${dbPort}/${dbSchema}`.replace(/:[^:]+@/, ':****@'), // Mask password in the output
       googlemaps: this.configService.get<string>(
         AppModuleConfigProperties.ENV_GOOGLE_MAPS
       )
