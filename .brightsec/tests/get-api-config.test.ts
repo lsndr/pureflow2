9c9672323d48dafdc2623caeb56a0cd9cc4154d5
Alexander <lsndrme@ya.ru>
2025-12-14 08:00:33 +0700

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/get-api-config.test.ts
+++ .brightsec/tests/get-api-config.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('GET /api/config', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['improper_asset_management', 'secret_tokens', 'csrf', 'xss'],
+      tests: ['secret_tokens'],
       attackParamLocations: [AttackParamLocation.PATH],
       starMetadata: {
         code_source: "lsndr/pureflow2:stable",
