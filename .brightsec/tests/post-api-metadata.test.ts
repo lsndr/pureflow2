68ce4e1ec311baf5d96d476e214da8a902a46d3b
Alexander <lsndrme@ya.ru>
2026-01-08 03:04:52 +0700

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-api-metadata.test.ts
@@ -0,0 +1,74 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /api/metadata', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['xxe', 'xss', 'secret_tokens'],
+      attackParamLocations: [AttackParamLocation.BODY, AttackParamLocation.HEADER],
+      starMetadata: {
+        code_source: "lsndr/pureflow:stable",
+        databases: ["PostgreSQL"],
+        user_roles: {
+          roles: [
+            "default-roles-pureflow",
+            "offline_access",
+            "uma_authorization",
+            "query-users",
+            "view-authorization",
+            "create-client",
+            "realm-admin",
+            "manage-users",
+            "manage-authorization",
+            "query-realms",
+            "view-events",
+            "manage-clients",
+            "view-realm",
+            "manage-realm",
+            "impersonation",
+            "query-clients",
+            "query-groups",
+            "manage-events",
+            "view-clients",
+            "view-identity-providers",
+            "view-users",
+            "manage-identity-providers",
+            "read-token",
+            "view-profile",
+            "manage-account-links",
+            "manage-account",
+            "manage-consent",
+            "view-applications",
+            "view-consent",
+            "delete-account"
+          ]
+        }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/metadata`,
+      body: "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" viewBox=\"0 0 915 585\"><g stroke-width=\"3.45\" fill=\"none\"><path stroke=\"#000\" d=\"M11.8 11.8h411v411l-411 .01v-411z\"/><path stroke=\"#448\" d=\"M489 11.7h415v411H489v-411z\"/></g></svg>",
+      headers: { 'Content-Type': 'text/xml' }
+    });
+});
