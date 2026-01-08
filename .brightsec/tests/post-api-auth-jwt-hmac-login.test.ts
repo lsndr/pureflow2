144da8e891cc8aa5f5264f860c67bea20b89b157
Alexander <lsndrme@ya.ru>
2026-01-08 13:15:24 +0700

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/post-api-auth-jwt-hmac-login.test.ts
@@ -0,0 +1,81 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('POST /api/auth/jwt/hmac/login', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['jwt', 'csrf', 'sqli'],
+      attackParamLocations: [AttackParamLocation.BODY, AttackParamLocation.HEADER],
+      starMetadata: {
+        code_source: "lsndr/pureflow:stable",
+        databases: ["PostgreSQL"],
+        user_roles: {
+          roles: [
+            "default-roles-pureflow",
+            "offline_access",
+            "uma_authorization",
+            "query-users",
+            "view-authorization",
+            "create-client",
+            "realm-admin",
+            "manage-users",
+            "manage-authorization",
+            "query-realms",
+            "view-events",
+            "manage-clients",
+            "view-realm",
+            "manage-realm",
+            "impersonation",
+            "query-clients",
+            "query-groups",
+            "manage-events",
+            "view-clients",
+            "view-identity-providers",
+            "view-users",
+            "manage-identity-providers",
+            "read-token",
+            "view-profile",
+            "manage-account-links",
+            "manage-account",
+            "manage-consent",
+            "view-applications",
+            "view-consent",
+            "delete-account"
+          ]
+        }
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.POST,
+      url: `${baseUrl}/api/auth/jwt/hmac/login`,
+      body: {
+        user: 'john',
+        password: 'Pa55w0rd',
+        op: 'basic'
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiam9obiIsImV4cCI6MTYwOTI3MzYwMH0.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+      }
+    });
+});
