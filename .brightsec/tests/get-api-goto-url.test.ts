19a03544edd37af00497fee86b79ab5106024f30
Alexander <lsndrme@ya.ru>
2026-01-14 02:54:24 +0700

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/get-api-goto-url.test.ts
+++ .brightsec/tests/get-api-goto-url.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('GET /api/goto?url=:url', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['unvalidated_redirect', 'xss', 'ssrf'],
+      tests: ['unvalidated_redirect'],
       attackParamLocations: [AttackParamLocation.QUERY],
       starMetadata: {
         code_source: "lsndr/pureflow2:stable",
