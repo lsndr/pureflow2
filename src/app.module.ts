928f26e80a7efe26994fcc2272e1e1870261c94f
Alexander <lsndrme@ya.ru>
2026-01-20 15:11:56 +0700

    fix: apply automated fixes for detected vulnerabilities

--- src/app.module.ts
+++ src/app.module.ts
@@ -33,11 +33,14 @@ import { ChatModule } from './chat/chat.module';
       isGlobal: true
     }),
     HttpClientModule,
-    GraphQLModule.forRoot<MercuriusDriverConfig>({
+    GraphQLModule.forRootAsync<MercuriusDriverConfig>({
       driver: MercuriusDriver,
-      graphiql: false, // Disable GraphiQL
-      autoSchemaFile: true,
-      introspection: false // Disable introspection
+      useFactory: async (configService: ConfigService) => ({
+        graphiql: false, // Disable GraphiQL
+        autoSchemaFile: true,
+        introspection: configService.get('NODE_ENV') !== 'production' // Disable introspection in production
+      }),
+      inject: [ConfigService],
     }),
     PartnersModule,
     EmailModule,
