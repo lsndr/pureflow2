cccf6ca44d6c567b0fc292b4f746d236463b79b1
Alexander <lsndrme@ya.ru>
2026-02-03 21:12:15 +0700

    fix: apply automated fixes for detected vulnerabilities

--- src/partners/partners.service.ts
+++ src/partners/partners.service.ts
@@ -67,11 +67,16 @@ export class PartnersService {
   }
 
   private getFormattedXMLOutput(xmlNodes): string {
-    return `${this.XML_HEADER}\n<root>\n${xmlNodes.join('\n')}\n</root>`;
+    return `${this.XML_HEADER}
+<root>
+${xmlNodes.join('\n')}
+</root>`;
   }
 
   getPartnersProperties(xpathExpression: string): string {
-    let xmlNodes = this.selectPartnerPropertiesByXPATH(xpathExpression);
+    // Sanitize the xpathExpression to prevent XPath Injection
+    const sanitizedXpathExpression = xpathExpression.replace(/['"&<>]/g, '');
+    let xmlNodes = this.selectPartnerPropertiesByXPATH(sanitizedXpathExpression);
 
     if (!Array.isArray(xmlNodes)) {
       this.logger.debug(
