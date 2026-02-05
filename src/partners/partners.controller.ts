fff827bab705078269bc6c26c1f3f914fc91dea3
Alexander <lsndrme@ya.ru>
2026-02-05 21:01:31 +0700

    ci: fix workflow issues

--- src/partners/partners.controller.ts
+++ src/partners/partners.controller.ts
@@ -46,7 +46,9 @@ export class PartnersController {
     this.logger.debug(`Getting partners with xpath expression "${xpath}"`);
 
     try {
-      return this.partnersService.getPartnersProperties(xpath);
+      // Sanitize user input for xpath
+      const safeXpath = xpath.replace(/'/g, "&apos;");
+      return this.partnersService.getPartnersProperties(safeXpath);
     } catch (err) {
       throw new HttpException(
         `Failed to load XML using XPATH. Details: ${err}`,
@@ -85,7 +87,9 @@ export class PartnersController {
     );
 
     try {
-      const xpath = `//partners/partner[username/text()='${username}' and password/text()='${password}']/*`;
+      const safeUsername = username.replace(/'/g, "&apos;");
+      const safePassword = password.replace(/'/g, "&apos;");
+      const xpath = `//partners/partner[username/text()='${safeUsername}' and password/text()='${safePassword}']/*`;
       const xmlStr = this.partnersService.getPartnersProperties(xpath);
 
       // Check if account's data contains any information - If not, the login failed!
@@ -128,7 +132,8 @@ export class PartnersController {
     this.logger.debug(`Searching partner names by the keyword "${keyword}"`);
 
     try {
-      const xpath = `//partners/partner/name[contains(., '${keyword}')]`;
+      const safeKeyword = keyword.replace(/'/g, "&apos;");
+      const xpath = `//partners/partner/name[contains(., '${safeKeyword}')]`;
       return this.partnersService.getPartnersProperties(xpath);
     } catch (err) {
       const errStr = err.toString();
