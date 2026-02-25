c4ef66ed1b4f92d7001d6760a409a8a1c1f4d307
Alexander <lsndrme@ya.ru>
2026-02-25 14:37:26 +0700

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/post-graphql-latest-products.test.ts
+++ .brightsec/tests/post-graphql-latest-products.test.ts
@@ -21,7 +21,7 @@ after(() => runner.clear());
 test('POST /graphql latestProducts', { signal: AbortSignal.timeout(timeout) }, async () => {
   await runner
     .createScan({
-      tests: ['sqli', 'csrf', 'graphql_introspection', 'xss'],
+      tests: ['graphql_introspection'],
       attackParamLocations: [AttackParamLocation.BODY],
       starMetadata: {
         code_source: 'lsndr/pureflow2:stable',
