b9de0e6b047d475a92f536462822536fe815f259
Alexander <lsndrme@ya.ru>
2026-05-18 21:27:18 +0400

    test: optimize security tests to focus on specific vulnerabilities
    skip-checks:true

--- .brightsec/tests/get-api-partners-partner-login.test.ts
+++ .brightsec/tests/get-api-partners-partner-login.test.ts
@@ -22,9 +22,7 @@ test('GET /api/partners/partnerLogin', { signal: AbortSignal.timeout(timeout) },
   await runner
     .createScan({
       tests: [
-        'xpathi',
-        'full_path_disclosure',
-        'secret_tokens'
+        'xpathi'
       ],
       attackParamLocations: [AttackParamLocation.QUERY],
       starMetadata: {
