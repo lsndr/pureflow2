af3bc48b958c2de3a70168051c39beeb3420ba88
Alexander <lsndrme@ya.ru>
2026-05-20 11:29:40 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/get-api-partners-search-partners.test.ts
@@ -0,0 +1,43 @@
+import { test, before, after } from 'node:test';
+import { SecRunner } from '@sectester/runner';
+import { AttackParamLocation, HttpMethod } from '@sectester/scan';
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+test('GET /api/partners/searchPartners?keyword=Walter', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: [
+        'xpathi',
+        'full_path_disclosure'
+      ],
+      attackParamLocations: [AttackParamLocation.QUERY],
+      starMetadata: {
+        code_source: 'lsndr/pureflow2:stable',
+        databases: ['PostgreSQL'],
+        user_roles: ['admin', 'user']
+      },
+      poolSize: +process.env.SECTESTER_SCAN_POOL_SIZE! || undefined
+    })
+    .setFailFast(false)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/api/partners/searchPartners?keyword=Walter`,
+      headers: { 'Accept': 'text/xml' }
+    });
+});
